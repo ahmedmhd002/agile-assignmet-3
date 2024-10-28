8e74d4ce7ca00f419cb91d06203ffe361915c7ac
ahmed <ahmedmohamed@gmail.com>
2024-10-28 22:06:08 +0300

    first commit

---
+++ 2100723ahmedmohamedelmahdy.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int summation(int a, int b) {
+    return a + b;
+}
+int main() {
+    int result = summation(5, 6);
+    cout << "The sum is: " << result ;
+
+    return 0;
+}
